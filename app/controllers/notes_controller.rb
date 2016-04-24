bccbb3268e72970adb697369aeb938c7117417f4
acoscosx <sintarou2013@gmail.com>
2016-04-24 11:06:24 +0900

    Add index and show

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -2,10 +2,19 @@ class NotesController < ApplicationController
   def new
   end
 
+  def index
+    @notes = Note.all
+  end
+
   def create
     @note = Note.new
     @note.title = params[:title]
     @note.content = params[:content]
     @note.save
+    redirect_to note_path(@note.id)
+  end
+
+  def show
+    @note = Note.find(params[:id])
   end
 end
