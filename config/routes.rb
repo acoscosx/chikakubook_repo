fea4c633b5936704022c9a2620ecc189352d4996
acoscosx <sintarou2013@gmail.com>
2016-04-24 11:55:49 +0900

    Add edit page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   post '/notes' => 'notes#create'
   get '/notes' => 'notes#index'
-  get 'notes/new'
+  get '/notes/new'
   get '/notes/:id' => 'notes#show', as: 'note'
+  get '/notes/:id/edit' => 'notes#edit', as: 'edit_note'
+  patch '/notes/:id' => 'notes#update', as: 'update_note'
   root 'home#top'
   get '/about' => 'home#about'
 end
